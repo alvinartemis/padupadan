5cc2b774ccf21fc78e83da711b8ebe2cd81ddd6d
keyashael <keyyaaa1@gmail.com>
2025-05-24 15:15:11 +0700

    setpreference

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/upload/detail/{id}', [UploadVideoController::class, 'showVideoDetail'])->name('upload.detail');
     Route::post('/upload/post-final/{id}', [UploadVideoController::class, 'finalPost'])->name('upload.final.post');
     Route::post('/upload/discard', [UploadVideoController::class, 'discardUpload'])->name('upload.discard');
-
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile'); // Route untuk halaman profil
+    Route::get('/set-preference', [SetPreferenceController::class, 'index'])->name('set_preference.index');
+    Route::get('/set-preference/{step}', [SetPreferenceController::class, 'showStep'])->name('set_preference.step');
+    Route::post('/set-preference/{step}', [SetPreferenceController::class, 'saveStep'])->name('set_preference.save_step');
+    Route::get('/quiz/countdown', [SetPreferenceController::class, 'showCountdown'])->name('set_preference.countdown');
+    Route::get('/set-preference/result', [SetPreferenceController::class, 'showResult'])->name('set_preference.result');
+    Route::post('/set-preference/complete', [SetPreferenceController::class, 'complete'])->name('set_preference.complete');
 });
 
 
@@ -31,13 +36,4 @@ Route::post('/stylist/logout', [StylistAuthController::class, 'logout'])->name('
 
 Route::middleware('auth:stylist')->group(function () {
     Route::get('/stylist/dashboard', [StylistAuthController::class, 'dashboard'])->name('stylist.dashboard');
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/set-preference', [SetPreferenceController::class, 'index'])->name('set_preference.index');
-    Route::get('/set-preference/{step}', [SetPreferenceController::class, 'showStep'])->name('set_preference.step');
-    Route::post('/set-preference/{step}', [SetPreferenceController::class, 'saveStep'])->name('set_preference.save_step');
-    Route::get('/quiz/countdown', [SetPreferenceController::class, 'showCountdown'])->name('set_preference.countdown');
-    Route::get('/set-preference/result', [SetPreferenceController::class, 'showResult'])->name('set_preference.result');
-    Route::post('/set-preference/complete', [SetPreferenceController::class, 'complete'])->name('set_preference.complete');
 });

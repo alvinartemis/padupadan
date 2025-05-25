6e083c70573cc03ae42048234aff5058b43557b3
Aathifah Dewantari <147472868+thifahdewantari@users.noreply.github.com>
2025-05-25 19:31:23 +0700

    route update

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\StylistAuthController;
 use App\Http\Controllers\SetPreferenceController;
 use App\Http\Controllers\BookmarkController;
 use App\Http\Controllers\EditProfileController;
+use App\Http\Controllers\DigitalWardrobeController;
 
 
 Route::get('/', function () {
@@ -58,4 +59,45 @@ Route::post('/settings/editprofile', [EditProfileController::class, 'update'])->
 Route::get('/settings/bookmark', [BookmarkController::class, 'bookmark'])->name('bookmark');
 Route::get('/settings/bookmark/item/{id}', [BookmarkController::class, 'showItem'])->name('bookmark.show_item');
 Route::post('/logout', [Controller::class, 'logout'])->name('logout');
+
+// Digital Wardrobe Route
+Route::get('/digital-wardrobe', [App\Http\Controllers\DigitalWardrobeController::class, 'index'])
+    ->name('digital.wardrobe.index')
+    ->middleware('auth'); // Assuming only authenticated users can access their wardrobe
+
+Route::get('/digital-wardrobe/create', [App\Http\Controllers\DigitalWardrobeController::class, 'create'])
+    ->name('digital.wardrobe.create')
+    ->middleware('auth');
+
+// Menangani submit foto dan menampilkan form detail
+Route::post('/digital-wardrobe/process-photo', [App\Http\Controllers\DigitalWardrobeController::class, 'processPhoto'])
+    ->name('digital.wardrobe.processPhoto')
+    ->middleware('auth');
+
+ // Menyimpan detail item ke database (ini akan jadi target form detail)
+Route::post('/digital-wardrobe/store', [App\Http\Controllers\DigitalWardrobeController::class, 'storeDetails'])
+    ->name('digital.wardrobe.storeDetails')
+    ->middleware('auth');
+
+Route::post('/digital-wardrobe/discard-temporary-photo', [App\Http\Controllers\DigitalWardrobeController::class, 'discardTemporaryPhoto'])
+    ->name('digital.wardrobe.discardTemporaryPhoto')
+    ->middleware('auth');
+
+Route::get('/digital-wardrobe/{koleksiPakaian}', [App\Http\Controllers\DigitalWardrobeController::class, 'show'])
+    ->name('digital.wardrobe.show')
+    ->middleware('auth');
+
+// Route untuk menampilkan form edit item pakaian (persiapan)
+Route::get('/digital-wardrobe/{koleksiPakaian}/edit', [App\Http\Controllers\DigitalWardrobeController::class, 'edit'])
+    ->name('digital.wardrobe.edit')
+    ->middleware('auth');
+
+Route::put('/digital-wardrobe/{koleksiPakaian}', [App\Http\Controllers\DigitalWardrobeController::class, 'update'])
+    ->name('digital.wardrobe.update')
+    ->middleware('auth');
+
+Route::delete('/digital-wardrobe/{koleksiPakaian}', [App\Http\Controllers\DigitalWardrobeController::class, 'destroy'])
+    ->name('digital.wardrobe.destroy')
+    ->middleware('auth');
+
 
